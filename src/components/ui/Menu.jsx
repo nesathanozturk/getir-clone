7d87a54495cf1b8693a2b6d5e7aea49dc336f559
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-07 21:45:41 +0300

    feat: added collapse to footer

--- src/components/ui/Menu.jsx
+++ src/components/ui/Menu.jsx
@@ -1,19 +1,49 @@
+import { useState, useEffect } from "react";
+import { Collapse } from "react-collapse";
+import { useWindowWidth } from "@react-hook/window-size";
+
 function Menu({ title, items }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const windowWidth = useWindowWidth();
+
+  const toggleCollapse = () => {
+    if (windowWidth < 768) {
+      setIsOpen(!isOpen);
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen && windowWidth < 768) {
+      setIsOpen(false);
+    }
+    if (!isOpen && windowWidth > 768) {
+      setIsOpen(true);
+    }
+  }, [windowWidth]);
+
   return (
     <section>
       <nav className="grid gap-y-2 md:gap-y-4">
-        <h6 className="text-lg text-primary-brand-color">{title}</h6>
-        <nav>
-          <ul className="grid gap-y-1 md:gap-y-2">
-            {items.map((item, key) => (
-              <li key={key}>
-                <a href="#" className="text-sm">
-                  {item.title}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
+        <h6
+          onClick={toggleCollapse}
+          className="text-lg text-primary-brand-color"
+        >
+          {title}
+        </h6>
+        <Collapse isOpened={isOpen}>
+          <nav>
+            <ul className="grid gap-y-1 md:gap-y-2">
+              {items.map((item, key) => (
+                <li key={key}>
+                  <a href="#" className="text-sm">
+                    {item.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </Collapse>
       </nav>
     </section>
   );
