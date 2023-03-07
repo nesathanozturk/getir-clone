6d92a8f24da0f8a9214e9e8a78db4e71b8d3a044
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-07 18:13:49 +0300

    style: edited cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from "react";
+import cardsData from "../api/cards.json";
+
 function Cards() {
-  return <div>Cards</div>;
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    setCards(cardsData);
+  }, []);
+
+  return (
+    <div className="grid grid-cols-3 gap-x-">
+      {cards.length &&
+        cards.map((card) => (
+          <div className="bg-white p-14 rounded-lg shadow-sm flex flex-col items-center text-center">
+            <img
+              className="w-[150px] h-[150px] mb-6"
+              src={card.image}
+              alt={card.title}
+            />
+            <h6 className="font-semibold text-lg text-primary-brand-color">
+              {card.title}
+            </h6>
+            <p className="text-sm text-gray-700 mt-2">{card.description}</p>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default Cards;
