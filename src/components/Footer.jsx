4619789aa90f9df77eb9e6be1fdda0a34206a896
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-07 18:23:22 +0300

    feat: created menus

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,98 @@
 function Footer() {
+  const menus = [
+    {
+      title: "Getir'i Keşfedin",
+      items: [
+        {
+          title: "Hakkımızda",
+        },
+        {
+          title: "Kariyer",
+        },
+        {
+          title: "Teknoloji Kariyerleri",
+        },
+        {
+          title: "İletişim",
+        },
+        {
+          title: "Sosyal Sorumluluk Projeleri",
+        },
+      ],
+    },
+    {
+      title: "Yardıma mı ihtiyacınız var?",
+      items: [
+        {
+          title: "Sıkça Sorulan Sorular",
+        },
+        {
+          title: "Kişisel Verilerin Korunması",
+        },
+        {
+          title: "Gizlilik Politikası",
+        },
+        {
+          title: "Kullanım Koşulları",
+        },
+        {
+          title: "Çerez Politikası",
+        },
+      ],
+    },
+    {
+      title: "İş Ortağımız Olun",
+      items: [
+        {
+          title: "Bayimiz Olun",
+        },
+        {
+          title: "Deponuzu Kiralayın",
+        },
+        {
+          title: "GetirYemek Restoranı Olun",
+        },
+        {
+          title: "GetirÇarşı İşletmesi Olun",
+        },
+        {
+          title: "Zincir Restoranlar",
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className="bg-white">
-      <div className="container mx-auto"></div>
+    <div className="bg-white mt-10">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-4 pt-10">
+          <section>
+            <nav className="grid gap-y-4">
+              <h6 className="text-lg text-primary-brand-color">
+                Getir'i indirin!
+              </h6>
+              <a href="#">
+                <img
+                  src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg"
+                  alt="App Store"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg"
+                  alt="Google Play Store"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg"
+                  alt="Huawei App Gallery"
+                />
+              </a>
+            </nav>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
