142208b8647638793a0f7db341bc6a451a983583
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-07 22:32:03 +0300

    style: edited margib

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,6 +1,6 @@
 function Title({ children }) {
   return (
-    <h3 className="text-sm font-semibold mb-3 px-6 md:px-8">{children}</h3>
+    <h3 className="text-sm font-semibold mb-1 px-6 md:px-5">{children}</h3>
   );
 }
 
