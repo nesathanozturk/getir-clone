8d299d735c8bee9328f6102ca850bf901d68225d
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-03-07 20:11:24 +0300

    style: responsive slider

--- src/components/Campaigns.jsx
+++ src/components/Campaigns.jsx
@@ -40,13 +40,41 @@ function Campaigns() {
     infinite: true,
     speed: 500,
     arrows: true,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3500,
     cssEase: "linear",
     nextArrow: <NextBtn />,
     prevArrow: <PrevBtn />,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+          arrows: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false,
+        },
+      },
+    ],
   };
 
   return (
